bb27dd3020070d9a2c3bbfa8992a40ed0ec142af
WLAXY <WLAXY@DESKTOP-3CSLSJ5>
2023-05-16 16:47:31 -0500

    Agregar archivos de proyecto.

---
+++ citasmedicascrud/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using citasmedicascrud.WS;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+
+namespace citasmedicascrud
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://127.19.96.1/citas/post.php";
+        private readonly HttpClient cliente = new HttpClient();
+        private ObservableCollection<Datos> post;
+                    
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        public async void obtener()
+        {
+            var content = await cliente.GetStringAsync(Url);
+            List<citasmedicascrud.WS.Datos> posts = JsonConvert.DeserializeObject<List<citasmedicascrud.WS.Datos>>(content);
+            post = new ObservableCollection<WS.Datos>(post);
+            lista.ItemsSource = post;
+
+        }
+
+        private void btnregistro_clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());//
+        }
+    }
+}
---
+++ citasmedicascrud/Registro.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace citasmedicascrud
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues("http://127.19.96.1/citas/post.php", "POST", parametros);
+                DisplayAlert("Alerta", "Dato Ingresado", "salir");
+             }
+
+            catch (Exception ex)
+            {
+
+                DisplayAlert("ALERTA", ex.Message, "cerrar");
+            }
+        }
+
+        private void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
