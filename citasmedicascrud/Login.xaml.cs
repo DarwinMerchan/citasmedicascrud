28d291b739e8a80bc7b6fc7fac59163da10a03c1
WLAXY <WLAXY@DESKTOP-3CSLSJ5>
2023-05-21 16:35:30 -0500

    ACTUALIZACIONES DE BOTONES

--- citasmedicascrud/Login.xaml.cs
+++ citasmedicascrud/Login.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.IO;
+using citasmedicascrud.Modelo;
+using Newtonsoft.Json;
 
 namespace citasmedicascrud
 {
@@ -28,20 +32,11 @@ namespace citasmedicascrud
             else
             {
                 //bota la alerta y se limpia el cuadro 
-                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "Cerrar");
+                await DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "Cerrar");
                 txtUsuario.Text = "";
                 txtContrasena.Text = "";
             }
-             }
-
-        private void btnRegistro_Clicked(object sender, EventArgs e)
-        {
-
         }
 
-        private void btnOcontrasena_Clicked(object sender, EventArgs e)
-        {
-
-        }
     }
 }
